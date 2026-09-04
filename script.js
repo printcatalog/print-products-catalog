b7d0244fc297c6d0090ed9d08afc644093d142c4
printcatalog <v.karra2004@gmail.com>
2026-09-04 13:59:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+    const accordions = document.querySelectorAll(".accordion"); accordions.forEach(function(button)
+    { button.addEventListener("click", function()
+    {
+        this.classList.toggle("active"); const
+        panel = this.nextElementSibling; if (panel.style.maxHeight) { panel.style.maxHeight = null; } else { panel.style.maxHeight = panel.scrollHeight + "px"; } }); });
+        </script>
+
+
